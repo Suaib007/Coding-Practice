99e40ebba67a582dce41a274b9dcc48f67935b77
Suaib007 <suaibsaifi07@gmail.com>
2024-06-24 16:21:36 +0530

    changes

--- function.cpp
+++ function.cpp
@@ -33,37 +33,47 @@ using namespace std;
 // return finalAns;
 // }
 
-void printCounting(int n)
-{
-   for (int i = 1; i <= n; i++)
-   {
-      cout << i << endl;
-   }
-}
+// void printCounting(int n)
+// {
+//    for (int i = 1; i <= n; i++)
+//    {
+//       cout << i << endl;
+//    }
+// }
 
-void checkEvenOdd(int num)
-{
-   if (num % 2 == 0)
-   {
-      cout << "Even Number" << endl;
-   }
-   else
-   {
-      cout << "Odd Number" << endl;
-   }
-}
+// void checkEvenOdd(int num)
+// {
+//    if (num % 2 == 0)
+//    {
+//       cout << "Even Number" << endl;
+//    }
+//    else
+//    {
+//       cout << "Odd Number" << endl;
+//    }
+// }
 
-void findSumUptoN(int n)
-{
-   int sum = 0;
-   for (int i = 0; i <= n; i++)
-   {
-      sum += i;
-   }
-   cout << "Final Sum : " << sum << endl;
-}
+// void findSumUptoN(int n)
+// {
+//    int sum = 0;
+//    for (int i = 0; i <= n; i++)
+//    {
+//       sum += i;
+//    }
+//    cout << "Final Sum : " << sum << endl;
+// }
 
-void findSumEvenUptoN(int n)
+// void findSumEvenUptoN(int n)
+// {
+//    int sum = 0;
+//    for (int i = 2; i <= n; i + 2)
+//    {
+//       sum += i;
+//    }
+//    cout << "Final Sum : " << sum << endl;
+// }
+
+void sumEvenNum(int n)
 {
    int sum = 0;
    for (int i = 2; i <= n; i + 2)
@@ -72,6 +82,9 @@ void findSumEvenUptoN(int n)
    }
    cout << "Final Sum : " << sum << endl;
 }
+// {
+//    cout << "Hello World!" << endl;
+// }
 
 int main()
 {
@@ -81,7 +94,10 @@ int main()
    // cout << "Maximum is :" << maximum << endl;
    // printCounting(10);
    // checkEvenOdd(101);
-   findSumEvenUptoN(10);
+   // findSumEvenUptoN(10);
    // sumEvenUptoN(130);
+   // printHello();
+   sumEvenNum(20);
+
    return 0;
 }
