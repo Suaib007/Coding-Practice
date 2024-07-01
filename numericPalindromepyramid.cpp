62203f76eb69d11cb1d062922422d6fc4de5de43
Suaib007 <suaibsaifi07@gmail.com>
2024-07-01 11:32:08 +0530

    NumericPalindromeEquilateralPyramid

--- numericPalindromepyramid.cpp
+++ numericPalindromepyramid.cpp
@@ -5,35 +5,52 @@ int main()
 {
    int n;
    cin >> n;
-   int k = n;
+   // int k = n;
 
+   // for (int row = 0; row < n; row++)
+   // {
+   //    int c = 1;
+   //    for (int col = 0; col < k; col++)
+   //    {
+   //       if (col < n - row - 1)
+   //       {
+   //          cout << " ";
+   //       }
+   //       else if (col <= n - 1)
+   //       {
+   //          cout << c;
+   //          c++;
+   //       }
+   //       else if (col == n)
+   //       {
+   //          c = c - 2;
+   //          cout << c;
+   //          c--;
+   //       }
+   //       else
+   //       {
+   //          cout << c;
+   //          c--;
+   //       }
+   //    }
+   //    k++;
+   //    cout << endl;
+   // }
    for (int row = 0; row < n; row++)
    {
-      int c = 1;
-      for (int col = 0; col < k; col++)
+      for (int col = 0; col < n - row - 1; col++)
       {
-         if (col < n - row - 1)
-         {
-            cout << " ";
-         }
-         else if (col <= n - 1)
-         {
-            cout << c;
-            c++;
-         }
-         else if (col == n)
-         {
-            c = c - 2;
-            cout << c;
-            c--;
-         }
-         else
-         {
-            cout << c;
-            c--;
-         }
+         cout << " ";
       }
-      k++;
+      for (int col = 0; col < row + 1; col++)
+      {
+         cout << col + 1;
+      }
+      for (int col = 0; col < row; col++)
+      {
+         cout << row - col;
+      }
       cout << endl;
    }
+   // cout << endl;
 }
