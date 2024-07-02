88ab8c7225bf1c52b1ed6df893c0e5e9d9b08882
Suaib007 <suaibsaifi07@gmail.com>
2024-07-02 11:05:23 +0530

    Given Integer N Count no. of Set Bit in it

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,25 +1,28 @@
-or (int row = 0; row < n; row++)
+#include <iostream>
+using namespace std;
+
+int reverse(int x)
+{
+   int ans = 0, rem = 0;
+   bool isNeg = false;
+   if (x <= INT_MIN)
    {
-
-      // space
-
-      for (int col = 0; col < row; col++)
-      {
-         cout << " ";
-      }
-
-      // star
-      int totalCol = n - row;
-      for (int col = 0; col < totalCol; col++)
-      {
-         if (col == 0 || col == totalCol - 1)
-         {
-            cout << "* ";
-         }
-         else
-         {
-            cout << "  ";
-         }
-      }
-      cout << endl;
+      return 0;
    }
+   if (x < 0)
+   {
+      isNeg = true;
+      x = -x;
+   }
+   while (x > 0)
+   {
+      if (ans > INT_MAX / 10)
+      {
+         return 0;
+      }
+      int digit = x % 10;
+      ans = ans * 10 + digit;
+      x = x / 10;
+   }
+   return isNeg ? -ans : ans;
+}
